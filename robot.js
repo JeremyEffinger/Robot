f3a5567fc992aeb5ba1320b28e1ce738460778a7
JeremyEffinger <Jeremy.Effinger@gmail.com>
2022-11-17 22:02:46 -0600

    Added locations, road graph func, and villag class

---
+++ robot.js
@@ -0,0 +1,63 @@
+const roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  "Marketplace-Farm",
+  "Marketpalce-Post Office",
+  "Marketplace-Shop",
+  "Marketplace-Town Hall",
+  "Shop-Town Hall",
+];
+
+let buildGraph = (edges) => {
+  let graph = Object.create(null); // create a null object.
+
+  let addEdge = (from, to) => {
+    //This function takes two inputs a from and a to.
+    if (graph[from] == null) {
+      //checks to see if our start is in the object.
+      graph[from] = [to]; //if not mape the from key to an array of places you can reach from there.
+    } else {
+      graph[from].push(to); //if from exists push the destination to the array holding locations you can get to.
+    }
+  };
+
+  for (let [from, to] of edges.map((r) => r.split("-"))) {
+    //split the array on - and map the parts as from and to.
+    addEdge(from, to); //run the add edge function with all the start points
+    addEdge(to, from); //map the other side for completeness
+  }
+
+  return graph; //return the object now full of key values of starting location with arrays of destinations you can get to.
+};
+
+const roadGraph = buildGraph(roads);
+let roadGraphString = JSON.stringify(roadGraph); //this is just a stringified version of the graph for error checking.
+console.log(roadGraphString);
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels
+        .map((p) => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter((p) => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
